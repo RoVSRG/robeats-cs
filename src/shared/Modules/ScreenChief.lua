3044c4fd271ddb39d4cd5249a9bd73d5565cfb86
astralkingdummy <cjansah11@gmail.com>
2025-07-25 12:44:49 -0400

    no more transititon animation

--- src/shared/Modules/ScreenChief.lua
+++ src/shared/Modules/ScreenChief.lua
@@ -38,48 +38,11 @@ function ScreenChief:GetScreenGui()
 end
 
 function ScreenChief:Switch(target)
-	local TweenService = game:GetService("TweenService")
-
 	local current: CanvasGroup = self:GetCurrentScreen()
 	local targetScreen: CanvasGroup = self:GetScreen(target)
 
-	-- Start with target screen offscreen to the left
-	targetScreen.Position = UDim2.fromScale(-1.5, 0) -- offscreen left
-	targetScreen.Parent = live
-
-	-- Transition durations
-	local tweenInfo = TweenInfo.new(0.25, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
-
-	-- Animate current screen off to the right
-	local outTween = TweenService:Create(current, tweenInfo, {
-		Position = UDim2.fromScale(2.5, 0) -- offscreen right
-	})
-
-	-- Animate target screen in from the left to center
-	local inTween = TweenService:Create(targetScreen, tweenInfo, {
-		Position = UDim2.fromScale(0, 0) -- center screen
-	})
-
-	-- Optional mask to prevent user interaction during transition
-	local transitionMask = Instance.new("TextButton")
-	transitionMask.Text = ""
-	transitionMask.Transparency = 1
-	transitionMask.Size = UDim2.fromScale(1,1)
-	transitionMask.Position = UDim2.fromScale(0.5, 0.5)
-	transitionMask.AnchorPoint = Vector2.new(0.5, 0.5)
-	transitionMask.BackgroundTransparency = 1
-	transitionMask.AutoButtonColor = false
-	transitionMask.ZIndex = 9999 -- make sure it's on top (another 9 for good measure)
-	transitionMask.Parent = live
-
-	outTween:Play()
-	inTween:Play()
-
-	inTween.Completed:Once(function()
-		current.Parent = screens -- hide old screen
-		targetScreen.Parent = live -- ensure target is visible (if not already)
-		transitionMask:Destroy()
-	end)
+	current.Parent = screens -- hide old screen
+	targetScreen.Parent = live -- ensure target is visible (if not already)
 end
 
 
