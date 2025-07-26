ab3d9a0add1aed339b89eeae86ac104416ad102a
kisperal <stirmanjr@gmail.com>
2025-07-26 14:26:26 -0800

    GHDSDHFGDFDSGFGHFSDGSGDF

--- src/state/Game.lua
+++ src/state/Game.lua
@@ -2,6 +2,11 @@ local Val = require(game.ReplicatedStorage.Libraries.Val)
 
 local Game = {}
 
+Game.results = Val.scope {
+    open = Val.new(false),
+    score = Val.new(nil)
+}
+
 Game.currentGame = Val.new(nil) -- Current active game instance
 
 return Game
