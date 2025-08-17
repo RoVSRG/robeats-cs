4e72c373bc7fdb262c3d3c6bcf6b8d59f074d233
kisperal <stirmanjr@gmail.com>
2025-08-17 10:39:18 -0800

    Fix bloom

--- src/client/Main.client.lua
+++ src/client/Main.client.lua
@@ -14,32 +14,45 @@ local GetSettings = game.ReplicatedStorage.Remotes.Functions.GetSettings
 local Transient = require(game.ReplicatedStorage.State.Transient)
 
 local function handlePlayerSettings()
-    local playerSettings = GetSettings:InvokeServer()
-    SettingsSerializer.apply_serialized_opts(playerSettings)
+	local playerSettings = GetSettings:InvokeServer()
+	SettingsSerializer.apply_serialized_opts(playerSettings)
 end
 
--- INITIALIZE GAME
+local function setupLighting()
+	local lighting = game:GetService("Lighting")
 
-EnvironmentSetup:initial_setup()
-
-local live = ScreenChief:GetScreenGui()
-
-local MainMenu = ScreenChief:GetScreen("Initialize")
-MainMenu.Parent = live
-
-for _, skin in require(game.ReplicatedStorage.Skins):key_itr() do
-    task.spawn(function()
-        ContentProvider:PreloadAsync({ skin })
-    end)
+	local bloom = Instance.new("BloomEffect")
+	bloom.Intensity = 0.1
+	bloom.Size = 24
+	bloom.Threshold = 2
+	bloom.Parent = lighting
 end
 
-SongDatabase:LoadAllSongs()
+local function initialize()
+	setupLighting()
 
-if not SongDatabase.IsLoaded then
-    SongDatabase.Loaded.Event:Wait()
+	EnvironmentSetup:initial_setup()
+
+	local live = ScreenChief:GetScreenGui()
+
+	local MainMenu = ScreenChief:GetScreen("Initialize")
+	MainMenu.Parent = live
+
+	for _, skin in require(game.ReplicatedStorage.Skins):key_itr() do
+		task.spawn(function()
+			ContentProvider:PreloadAsync({ skin })
+		end)
+	end
+
+	SongDatabase:LoadAllSongs()
+
+	if not SongDatabase.IsLoaded then
+		SongDatabase.Loaded.Event:Wait()
+	end
+
+	Transient.song.selected:set(math.random(1, #SongDatabase.songs))
+
+	handlePlayerSettings()
 end
 
-Transient.song.selected:set(math.random(1, #SongDatabase.songs))
-
--- Load player settings
-handlePlayerSettings()
+initialize()
