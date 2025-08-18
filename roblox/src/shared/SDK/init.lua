cca1e2e57140147cdbb1b387fb3fa562e0d6f77c
kisperal <stirmanjr@gmail.com>
2025-08-18 01:15:54 -0800

    Fix

--- roblox/src/shared/SDK/init.lua
+++
@@ -1,9 +0,0 @@
--- Auto-generated SDK - DO NOT EDIT MANUALLY
--- Generated at: 2025-08-18T01:54:53.216Z
-
-local SDK = {}
-
-SDK.Players = require(script.Players)
-SDK.Scores = require(script.Scores)
-
-return SDK
