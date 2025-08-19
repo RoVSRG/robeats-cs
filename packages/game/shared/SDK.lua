ad1301ed418f51d5b2c4b75d29db7f50bdfff4f5
kisperal <stirmanjr@gmail.com>
2025-08-19 13:17:11 -0800

    sdgfdgfsgdf

---
+++ packages/game/shared/SDK.lua
@@ -0,0 +1,10 @@
+local sdkModule = script.Parent:FindFirstChild("_sdk_bin")
+
+if not sdkModule then
+	warn(
+		"SDK module not found. Please run `npm run sdk:generate` to generate the SDK. Online features will be disabled."
+	)
+	return
+end
+
+return require(sdkModule)
